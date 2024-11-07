ad1cc9deb2166d2c6ed4cb6e3607e32049571d37
Natalia Suarez Cortes <nsuarez35@misena.edu.co>
2024-11-07 10:44:02 -0500

    Project

---
+++ src/test/java/com/example/SoftgymSpring/SoftgymSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SoftgymSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftgymSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
